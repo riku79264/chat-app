2b481a2afd6575717cd5429b8a14078516517594
riku79264 <riku.tanimoto.tani@gmail.com>
2022-05-30 00:05:08 +0900

    Active Storageの導入

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -22,6 +22,7 @@ class MessagesController < ApplicationController
 
   def message_params
     params.require(:message).permit(:content).merge(user_id: current_user.id)
+    params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
   end
 end
 
